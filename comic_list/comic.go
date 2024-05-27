b644fe729782204d45b6d31ffb63fc5a463ea3a8
benjaminchin <benjamin.d.chin@gmail.com>
2024-05-27 02:59:52 -0400

    in theory works

--- comic_list/comic.go
+++ comic_list/comic.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "encoding/csv"
+    "strconv"
+    "fmt"
     "os"
 )
 
@@ -13,23 +15,114 @@ type Comic struct {
     cost string
 }
 
-type Comic_List struct {
-    list []Comic
-    size int
-    count int
-}
 
-func LoadCsv(filename string) ([][]string, error) {
+func LoadCsv(output *os.File, list []Comic, filename string) error {
     file, err := os.Open(filename)
+
     if err != nil {
-        return nil, err
+        return err
     }
+
+    defer file.Close()
 
     reader := csv.NewReader(file)
     records, err := reader.ReadAll()
     if err != nil {
-        return nil, err
+        return err
     }
 
-    return records, nil
+    for _, record := range records {
+        var c Comic
+        UpdateComic(&c, record)
+        AddComic(list, c)
+    }
+
+    fmt.Fprintf(output, "Number of comics: %d\n", len(list))
+    return nil
+}
+
+func AddComic(list []Comic, c Comic) {
+    list = append(list, c)
+}
+
+func UpdateComic(c *Comic, record []string) {
+    c.date = record[0]
+    c.code = record[1]
+    c.pub = record[2]
+    c.title = record[3]
+    c.cost = record[4]
+}
+
+func FindComic(out *os.File, list []Comic, index int) {
+    if index < len(list) {
+        DisplayComic(out, list[index])
+    }
+}
+
+func DisplayComic(out *os.File, c Comic) {
+    fmt.Fprintf(out, "Date: %s\n", c.date)
+    fmt.Fprintf(out, "Code: %s\n", c.code)
+    fmt.Fprintf(out, "Publisher: %s\n", c.pub)
+    fmt.Fprintf(out, "Title: %s\n", c.title)
+    fmt.Fprintf(out, "Cost: %s\n", c.cost)
+}
+
+func DisplayComicHorizontal(out *os.File, c Comic) {
+    fmt.Fprintf(out, "%s,%s,%s,%s,%s\n", c.date, c.code, c.pub, c.title, c.cost)
+}
+
+func DisplayList(out *os.File, list []Comic) {
+    if len(list) == 0 {
+        fmt.Fprintf(out, "List is currently empty.\n")
+    }
+
+    for i, c := range list {
+        fmt.Fprintf(out, "Comic Number: %d\n", i)
+        DisplayComic(out, c)
+    }
+}
+
+func DisplayListHorizontal(out *os.File, list []Comic) {
+    fmt.Fprintf(out, "DATE,CODE,PUBLISHER,TITLE,PRICE\n")
+
+    for _, c := range list {
+        DisplayComicHorizontal(out, c)
+    }
+}
+
+func RemoveAt(list []Comic, index int) bool {
+    if index <= len(list) {
+        list = append(list[:index], list[index+1:]...)
+        return true
+    }
+    return false
+}
+
+func BuyComic(list []Comic, cart []Comic, index int) {
+    if index < len(list) {
+        var c Comic = list[index]
+        AddComic(cart, c)
+    }
+}
+
+func Checkout(out *os.File, cart []Comic) {
+    fmt.Fprintf(out, "Comics in Purchase List\n")
+    subtotal := 0.0
+
+    for i, c := range cart {
+        if c.cost != "AR" {
+            cost, err := strconv.ParseFloat(c.cost, 64)
+            check(err)
+            subtotal += cost
+        }
+        fmt.Fprintf(out, "Comic Number: %d\n", i + 1)
+        DisplayComic(out, c)
+    }
+
+    tax := subtotal * 0.05
+    total := subtotal + tax
+
+    fmt.Fprintf(out, "Subtotal: %.2f\n", subtotal)
+    fmt.Fprintf(out, "\tTax: %.2f\n", tax)
+    fmt.Fprintf(out, "\tTax: %.2f\n", total)
 }
--- comic_list/main.go
+++ comic_list/main.go
@@ -3,12 +3,17 @@ package main
 import (
     "fmt"
     "os"
+    "strings"
+    "bufio"
+    "strconv"
 )
+
+const initSize = 10
 
 func main() {
     args := os.Args[1:]
 
-    if len(args) != 3 {
+    if len(args) != 2 {
         fmt.Fprintf(os.Stderr, "Usage: comics <input-file> <output-file>\n")
         os.Exit(1)
     }
@@ -16,18 +21,79 @@ func main() {
     fmt.Println(args)
 
     // Open input and output files for read and write
-//    output, err := os.Open(args[2])
-//    check(err)
-//
-//    input, err := os.Open(args[1])
-//    check(err)
-//
-    // Create Comic_Lists for all loaded comics, and bought comics
-    records, err := LoadCsv(args[0])
+    output, err := os.Open(args[1])
     check(err)
+    defer output.Close()
 
-    for _, record := range records {
-        fmt.Println(record)
+    input, err := os.Open(args[0])
+    check(err)
+    defer input.Close()
+
+    list := make([]Comic, initSize)
+
+    cart := make([]Comic, initSize)
+
+    scanner := bufio.NewScanner(input)
+
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        fields := strings.Fields(line)
+
+        command := fields[0]
+
+        if command == "load" {
+            // Get arg for load command
+            csv := fields[1]
+
+            fmt.Fprintf(output, "Command: %s %s\n", command, csv)
+
+            // Run command
+            err := LoadCsv(output, list, csv)
+            check(err)
+        } else if command == "buy" {
+            buy, err := strconv.ParseInt(fields[1], 10, 32)
+            check(err)
+
+            fmt.Fprintf(output, "Command: %s %d\n", command, buy)
+
+            BuyComic(list, cart, int(buy))
+            fmt.Fprintf(output, "Comic #%d added to purchase list\n", buy)
+        } else if command == "checkout" {
+            fmt.Fprintf(output, "Command: %s\n", command)
+
+            Checkout(output, cart)
+        } else if command == "display" {
+            fmt.Fprintf(output, "Command: %s\n", command)
+
+            DisplayList(output, list)
+        } else if command == "save" {
+            filename := args[1]
+
+            fmt.Fprintf(output, "Command: %s %s\n", command, filename)
+            save, err := os.Open(filename)
+            defer save.Close()
+            check(err)
+
+            DisplayListHorizontal(save, list)
+        } else if command == "clear" {
+            fmt.Fprintf(output, "Command: %s\n", command)
+            list = make([]Comic, initSize)
+        } else if command == "find" {
+            search, err := strconv.ParseInt(args[1], 10, 32)
+            check(err)
+
+            fmt.Fprintf(output, "Command: %s %d\n", command, search)
+
+            FindComic(output, list, int(search))
+        } else if command == "remove" {
+            remove, err := strconv.ParseInt(args[1], 10, 32)
+            check(err)
+
+            fmt.Fprintf(output, "Command %s %d\n", command, remove)
+
+            RemoveAt(list, int(remove))
+        }
     }
 }
 
